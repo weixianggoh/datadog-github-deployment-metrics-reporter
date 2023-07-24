250c6803b7cb3a0a942c087d39591294a04ee3b2
Wei Xiang Goh <weixianggoh1995@gmail.com>
2023-07-24 17:44:07 +0800

    fix: script with getLatestRelease

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 const core = require('@actions/core');
 const axios = require('axios');
 
-// async function getLatestRelease(githubPat, githubRepository) {
-//   try {
-//     const [owner, repo] = githubRepository.split('/');
-//     const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/releases/latest`, {
-//       headers: {
-//         'Accept': 'application/vnd.github+json',
-//         'Authorization': `Bearer ${githubPat}`,
-//         'X-GitHub-Api-Version': '2022-11-28',
-//         'Content-Type': 'application/json',
-//       }
-//     });
-//     return response.data.tag_name;
-//   } catch (error) {
-//     console.error('Error getting latest release:', error.response.data);
-//     throw error;
-//   }
-// }
+async function getLatestRelease(githubPat, githubRepository) {
+  try {
+    const [owner, repo] = githubRepository.split('/');
+    const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/releases/latest`, {
+      headers: {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': `Bearer ${githubPat}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+        'Content-Type': 'application/json',
+      }
+    });
+    return response.data.tag_name;
+  } catch (error) {
+    console.error('Error getting latest release:', error.response.data);
+    throw error;
+  }
+}
 
 async function run() {
   try {
     const ddApiKey = core.getInput('DD_API_KEY');
     const buildStatus = core.getInput('BUILD_STATUS');
     const githubRepository = core.getInput('GITHUB_REPOSITORY');
-    // const githubPat = core.getInput('GITHUB_PAT');
+    const githubPat = core.getInput('GITHUB_PAT');
     const timestamp = Math.floor(Date.now() / 1000);
 
-    // // Get the latest semantic release version
-    // const latestBuildVersion = await getLatestRelease(githubPat, githubRepository) ?? 'unknown';
+    // Get the latest semantic release version
+    const latestBuildVersion = await getLatestRelease(githubPat, githubRepository) ?? 'unknown';
+
+    let metricName;
+    if (buildStatus === 'success') {
+      metricName = 'build.success';
+    } else {
+      metricName = 'build.failure';
+    }
 
     // Send metric to Datadog
     await axios.post("https://api.datadoghq.com/api/v1/series", {
       "series": [
         {
-          "metric": `build:${buildStatus}`,
+          "metric": metricName,
           "points": [
             [timestamp, 1]
           ],
           "type": "count",
-          "tags": ["github:actions", `build:${buildStatus}`, `repo:${githubRepository}`]
+          "tags": ["github:actions", `build:${buildStatus}`, `repo:${githubRepository}`, `version:${latestBuildVersion}`]
         }
       ]
     }, {
